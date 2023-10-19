23707a098161ae190cc213e7e98322f757b6e771
Liam <ml646@duke.edu>
2023-10-18 20:55:14 -0400

    uodate makefile

--- rscopy/src/main.rs
+++ rscopy/src/main.rs
@@ -5,10 +5,9 @@ use std::env;
 
 fn main() {
     let start_time = std::time::Instant::now();
-    let current_dir = env::current_dir().unwrap();
-    println!("Current directory: {:?}", current_dir);
-
     let csv_file = "../taker_buy_sell_volume.csv";
+    let end_time = std::time::Instant::now();
+    let processing_time = end_time.duration_since(start_time);
     match read_csv_to_df(csv_file) {
         Ok(data) => {
             describe_data(&data);
@@ -17,7 +16,6 @@ fn main() {
             println!("Error reading CSV: {:?}", err);
         }
     }
-    let end_time = std::time::Instant::now();
-    let processing_time = end_time.duration_since(start_time);
+
     println!("Processing time: {:?}", processing_time);
 }
